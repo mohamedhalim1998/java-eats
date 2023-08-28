4966df89c0826913fdd4053b8ce7658fbddeafa2
Mohamed Halim <mohamedhalim1998@gmail.com>
2023-08-28 06:28:07 +0300

    initial commit

---
+++ src/main/java/com/mohamed/halim/javaeats/JavaeatsApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.halim.javaeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaeatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohamed/halim/javaeats/JavaeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.halim.javaeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
