d315ea1e5506b4957a4ce5b43f2fc178cb11eb53
Mohamed Halim <mohamedhalim1998@gmail.com>
2024-03-06 21:53:09 +0200

    ADD: login validation

--- src/main/java/com/mohamed/halim/javaeats/dto/request/UserLogin.java
+++ src/main/java/com/mohamed/halim/javaeats/dto/request/UserLogin.java
@@ -1,5 +1,6 @@
 package com.mohamed.halim.javaeats.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class UserLogin {
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
 }
