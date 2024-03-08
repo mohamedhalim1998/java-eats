c083deea86b7882e3590acae896b22a7702b980f
Mohamed Halim <mohamedhalim1998@gmail.com>
2024-03-08 17:53:00 +0200

    ADD: audit listener

--- src/main/java/com/mohamed/halim/javaeats/model/Restaurant.java
+++ src/main/java/com/mohamed/halim/javaeats/model/Restaurant.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Restaurant {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
