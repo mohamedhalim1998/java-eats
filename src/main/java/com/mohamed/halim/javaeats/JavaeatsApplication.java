aabee07b5d0bf0d35f8c83f96a56a0414a1e75df
Mohamed Halim <mohamedhalim1998@gmail.com>
2024-03-08 17:54:01 +0200

    ADD: some test data in start up

--- src/main/java/com/mohamed/halim/javaeats/JavaeatsApplication.java
+++ src/main/java/com/mohamed/halim/javaeats/JavaeatsApplication.java
@@ -1,7 +1,11 @@
 package com.mohamed.halim.javaeats;
 
+import com.mohamed.halim.javaeats.model.Restaurant;
+import com.mohamed.halim.javaeats.repository.RestaurantRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -11,5 +15,18 @@ public class JavaeatsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaeatsApplication.class, args);
 	}
+	@Bean
+	public CommandLineRunner initData(RestaurantRepository restaurantRepository) {
+		return args -> {
+            var restaurant = new Restaurant();
+            restaurant.setName("test");
+            restaurant.setDescription("test desc");
+            restaurant.setLocation("test loc");
+            for (int i = 0; i < 100; i++) {
+                restaurant.setId((long) i);
+                restaurantRepository.save(restaurant);
+            }
+        };
 
+    }
 }
