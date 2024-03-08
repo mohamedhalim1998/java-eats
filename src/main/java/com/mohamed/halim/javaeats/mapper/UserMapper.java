afc87cefa208aaeb91969a3e559716d9583713f1
Mohamed Halim <mohamedhalim1998@gmail.com>
2024-03-08 18:32:56 +0200

    EDIT: imports clean up

--- src/main/java/com/mohamed/halim/javaeats/mapper/UserMapper.java
+++ src/main/java/com/mohamed/halim/javaeats/mapper/UserMapper.java
@@ -2,15 +2,9 @@ package com.mohamed.halim.javaeats.mapper;
 
 import com.mohamed.halim.javaeats.dto.request.UserRegistration;
 import com.mohamed.halim.javaeats.dto.response.UserDto;
-import com.mohamed.halim.javaeats.jwt.JwtService;
 import com.mohamed.halim.javaeats.model.AppUser;
-import org.mapstruct.AfterMapping;
-import org.mapstruct.BeforeMapping;
-import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
 public abstract class UserMapper {
